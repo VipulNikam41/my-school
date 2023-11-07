8a1f115e80c7908c2520d2045f855fb8ca2cafbf
Vipul Nikam <vipul@Vipuls-Mac.local>
2023-11-07 23:25:44 +0530

    rebased

--- src/main/java/com/myscool/response/GetUserResponse.java
+++
@@ -1,9 +0,0 @@
-package com.myscool.response;
-
-import com.myscool.domain.dto.UserProfileDTO;
-import lombok.Data;
-
-@Data
-public class GetUserResponse extends BaseResponse {
-    private UserProfileDTO userProfileDTO;
-}
