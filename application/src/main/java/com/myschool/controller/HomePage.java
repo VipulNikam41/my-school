df9e516d10db95f1adf9f38aa3ec3407e656eb77
Vipul Nikam <vipul@Vipuls-Mac.local>
2023-12-31 19:38:27 +0530

    minor

--- application/src/main/java/com/myschool/controller/HomePage.java
+++ application/src/main/java/com/myschool/controller/HomePage.java
@@ -1,26 +1,14 @@
 package com.myschool.controller;
 
-import com.myschool.commons.dto.UserResponse;
-import com.myschool.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 public class HomePage {
-    private final UserService userService;
-
     @GetMapping("/home")
     public String home() {
         return "Home Page";
-    }
-
-    @GetMapping("/profile/{userId}/get")
-    public UserResponse getUser(@PathVariable UUID userId) {
-        return userService.getUserById(userId);
     }
 }
